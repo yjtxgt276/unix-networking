cdc3d402a6618424ff5113042e0007c491086167
xia zhang <xzhang2@linux60801.dc.engr.scu.edu>
2015-02-25 15:45:37 -0800

    push from dc

--- prj3/prj3_util.c
+++ prj3/prj3_util.c
@@ -22,7 +22,7 @@ int valid_cmd(char* str){
 }
 
 int display(char* data){
-    printf("sv_display: result from server:\n");
+    printf("Display: result from server:\n");
     printf("%s\n",data);
     return 0;
 }
@@ -32,26 +32,26 @@ int process_cmd(char* str, char* rslt){
     char *token,input[MAX_LEN], cmd[CMD_LEN],fname[FN_LEN]; 
     strncpy(input,str,MAX_LEN);
     if(NULL == (token = strtok(input," \n"))){
-	printf("sv_process_cmd:empty cmd\n"); 
+	printf("Process_cmd:empty cmd\n"); 
 	return -1;
     }
     else
 	strcpy(cmd, token);
     if(NULL == (token = strtok(NULL, " \n"))){
-	printf("sv_process_cmd:empty file name\n"); 
+	printf("Process_cmd:empty file name\n"); 
 	strcpy(rslt, "from server: please input a file name\n");
 	return -1;
     }
     else
 	strcpy(fname, token);
     if( 0==strcmp("read",cmd) ){
-	printf("sv_process_cmd: reading file content...\n");
+	printf("Process_cmd: reading file content...\n");
 	read_file(rslt,fname);
     }
     else if( 0==strcmp("delete",cmd) ){
-	printf("sv_process_cmd: deleting file ...\n");
+	printf("Process_cmd: deleting file ...\n");
 	if(remove(fname)==-1){
-	    perror("sv_server:delete file");	
+	    perror("Server:delete file");	
 	    strcpy(rslt, "from server: couldnot delete the file\n");
 	}
 	else
@@ -59,7 +59,7 @@ int process_cmd(char* str, char* rslt){
 	    strcpy(rslt,"from server: file deleted successfully\n");
     }
     else{
-	printf("sv_process_cmd: invalid command\n"); 
+	printf("Process_cmd: invalid command\n"); 
 	return -1;
     }
     return 0;
@@ -69,14 +69,14 @@ int read_file(char* dest, char* fname){
     FILE* fp;
     char c;
     if(NULL == (fp = fopen(fname,"r"))){
-	perror("sv_server: open file"); 
+	perror("server: open file"); 
 	strcpy(dest,"from server: couldnot open the file\n");
 	return -1;
     }
     int i =0;
     while(EOF != (c=fgetc(fp))){
 	if(i == SHM_SIZE){
-	    printf("sv_server: file too large\n");
+	    printf("server: file too large\n");
 	    strcpy(dest, "from server: file too large\n");
 	    return -1;
 	} 
